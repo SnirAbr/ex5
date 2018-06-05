1ff8ed0ec3ad18ae42901045c40074764dee79bb
SnirAbr <snir987654@gmail.com>
2018-06-05 23:13:38 +0300

    added size filters

--- src/filesprocessing/filters/AllFilter.java
+++ src/filesprocessing/filters/AllFilter.java
@@ -6,20 +6,21 @@ import java.util.ArrayList;
 /**
  * a class of a filter which gives only files who are either executable or not (depends on input)
  */
-public class AllFilter extends Filter{
+public class AllFilter extends Filter {
 
     /**
      * constructs a new filter
+     *
      * @param sourceDir the directory of the files
      */
-    public AllFilter(String sourceDir){
+    public AllFilter(String sourceDir) {
         super(sourceDir);
     }
 
     @Override
     public ArrayList<File> filter(String[] args) {
         //TODO handle exceptions
-        if(args.length != 0){
+        if (args.length != 0) {
         }
         return allFiles;
     }
