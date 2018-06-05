0ba9e5075a7dccbc300bfc1ee83fafbea2573fee
omer5198 <omer8915@gmail.com>
2018-06-05 23:46:42 +0300

    added folders and updated packages

---
+++ src/filesprocessing/analysys/factories/filters/SizeFilter.java
@@ -0,0 +1,92 @@
+package filesprocessing.analysys.factories.filters;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+/**
+ * a class of a filter which gives only files larger than an asked number
+ */
+public class SizeFilter extends Filter {
+
+    /**
+     * enum of possible types of size filters
+     */
+    public enum SIZE_TYPE {
+        GREATER, BETWEEN, SMALLER
+    }
+
+    /**
+     * this filter's filter type
+     */
+    private SIZE_TYPE filterType;
+
+    /**
+     * constructs a new filter
+     *
+     * @param sourceDir the directory of the files
+     * @param filterType type of this filter
+     */
+    public SizeFilter(String sourceDir, SIZE_TYPE filterType) {
+        super(sourceDir);
+        this.filterType = filterType;
+    }
+
+    @Override
+    public ArrayList<File> filter(String[] args) {
+        //TODO handle exceptions
+        if (args.length != 1) {
+        }
+        try {
+            int numBytes = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+        }
+        ArrayList<File> goodFiles = new ArrayList<File>();
+        Predicate<File> filter = createFilter(args);
+        for (File file : allFiles) {
+            if (filter.test(file)) {
+                goodFiles.add(file);
+            }
+        }
+        return goodFiles;
+    }
+
+    private Predicate<File> createFilter(String[] args) {
+        Predicate<File> filter = null;
+        switch (filterType) {
+            case GREATER:
+                filter = new Predicate<File>() {
+                    int lowerBound = Integer.parseInt(args[0]);
+
+                    @Override
+                    public boolean test(File file) {
+                        return file.length() > lowerBound;
+                    }
+                };
+                break;
+            case BETWEEN:
+                filter = new Predicate<File>() {
+                    int lowerBound = Integer.parseInt(args[0]);
+                    int upperBound = Integer.parseInt(args[1]);
+
+                    @Override
+                    public boolean test(File file) {
+                        return file.length() >= lowerBound && file.length() <= upperBound;
+                    }
+                };
+                break;
+            case SMALLER:
+                filter = new Predicate<File>() {
+                    int upperBound = Integer.parseInt(args[0]);
+
+                    @Override
+                    public boolean test(File file) {
+                        return file.length() < upperBound;
+                    }
+                };
+                break;
+
+        }
+        return filter;
+    }
+}
---
+++ src/filesprocessing/analysys/factories/orders/Order.java
@@ -0,0 +1,29 @@
+package filesprocessing.analysys.factories.orders;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+abstract public class Order {
+
+	/**
+	 * list of files that need to be ordered
+	 */
+	protected final ArrayList<File> allFiles;
+
+	/**
+	 * constructs a filter with file directory
+	 * @param files an array list of files
+	 */
+	public Order(ArrayList<File> files){
+		// todo Check for nullity
+		allFiles = files;
+	}
+
+	/**
+	 * order 'allFiles' and return a list of ordered files
+	 * @return an array of File objects of the filtered list
+	 */
+	abstract public ArrayList<File> order();
+
+}
