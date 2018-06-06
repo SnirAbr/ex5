a62055a4cebedd09f1ca7917aa8779e6c6051ec1
SnirAbr <snir987654@gmail.com>
2018-06-07 00:50:10 +0300

    addded parser

--- src/filesprocessing/DirectoryProcessor.java
+++ src/filesprocessing/DirectoryProcessor.java
@@ -2,8 +2,37 @@ package filesprocessing;
 
 import filesprocessing.analysys.Parser;
 
+import java.util.ArrayList;
+
 public class DirectoryProcessor {
+
+    private static final String NUM_ARGS_ERR = "ERROR: the number of arguments is different than 2";
+
     public static void main(String[] args) {
-        System.out.println(Parser.processFiles(args[0], args[1]));
+        if (args.length != 2) {
+            System.err.println(NUM_ARGS_ERR);
+            return;
+        }
+        ArrayList<OutputLine> output = Parser.processFiles(args[0], args[1]);
+        for (OutputLine line : output) {
+            switch (line.getStatus()) {
+                case ERROR:
+                    printError(line);
+                    return;
+                case WARNING:
+                    printWarning(line);
+                    break;
+                case VALID:
+                    System.out.println(line);
+            }
+        }
+    }
+
+    private static void printError(OutputLine err) {
+        System.err.println(err);
+    }
+
+    private static void printWarning(OutputLine warning) {
+        System.err.println(warning);
     }
 }
--- src/filesprocessing/OutputLine.java
+++ src/filesprocessing/OutputLine.java
@@ -2,29 +2,37 @@ package filesprocessing;
 
 public class OutputLine {
 
-	/**
-	 * enum of available statuses
-	 */
-	public enum STATUS {
-		VALID, WARNING, ERROR
-	}
-	
-	private String text;
-	
-	private STATUS status;
+    /**
+     * enum of available statuses
+     */
+    public enum STATUS {
+        VALID, WARNING, ERROR
+    }
 
-	/**
-	 * Class' Constructor
-	 * @param text the text of the line
-	 * @param status the status of the line
-	 */
-	public OutputLine(String text, STATUS status) {
-		this.text = text;
-		this.status = status;
-	}
+    private String text;
 
-	@Override
-	public String toString() {
-		return this.text;
-	}
+    private STATUS status;
+
+    /**
+     * Class' Constructor
+     *
+     * @param text   the text of the line
+     * @param status the status of the line
+     */
+    public OutputLine(String text, STATUS status) {
+        this.text = text;
+        this.status = status;
+    }
+
+    /**
+     * @return the status of this line
+     */
+    public STATUS getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
 }
